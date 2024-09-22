e6599364d55c5a8d09cf40c796e71ddd42095d1f
arepo90 <Arepo90@proton.me>
2024-09-22 16:35:57 -0600

    2nd

--- client.cpp
+++ client.cpp
@@ -1,7 +1,8 @@
-#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <opencv2/opencv.hpp>
 #include <sys/types.h>
 #include <winsock2.h>
+#include <windows.h>
 #pragma comment(lib, "ws2_32.lib")
 
 using namespace std;
@@ -19,68 +20,89 @@ int main(){
     SOCKET client_socket;
     struct sockaddr_in server_addr;
     char recv_buffer[BUFFER_SIZE];
+    int attempt = 1;
 
-    if(WSAStartup(MAKEWORD(2,2), &wsaData) != 0){
-        cout << "Failed to initialize Winsock. Error Code: " << WSAGetLastError() << '\n';
-        return 1;
-    }
-    if((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET){
-        cout << "Socket creation failed. Error Code: " << WSAGetLastError() << '\n';
-        return 1;
-    }
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
-    server_addr.sin_port = htons(PORT);
+    while(1){
+        cout << "[i] Initializing client...\n";
+        if(WSAStartup(MAKEWORD(2,2), &wsaData) != 0){
+            cout << "[e] Failed to initialize Winsock. Error Code: " << WSAGetLastError() << '\n';
+            return 1;
+        }
+        if((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET){
+            cout << "[e] Socket creation failed. Error Code: " << WSAGetLastError() << '\n';
+            return 1;
+        }
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+        server_addr.sin_port = htons(PORT);
 
-    if(connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR){
-        cout << "Connection failed. Error Code: " << WSAGetLastError() << '\n';
+        if(connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR){
+            cout << "[e] Connection failed. Error Code: " << WSAGetLastError() << '\n';
+            closesocket(client_socket);
+            cout << "[w] Attempt " << attempt << ". Restarting in 3 seconds...\n";
+            Sleep(3000);
+            attempt++;
+            continue;
+        }
+        cout << "[i] Connected to server" << '\n';
+
+        cout << "[i] Initializing capture device...\n";
+        VideoCapture cap(0);
+        if(!cap.isOpened()){
+            cout << "[e] Error opening camera" << '\n';
+            return 1;
+        }
+        cap.set(CAP_PROP_FRAME_WIDTH, WIDTH);
+        cap.set(CAP_PROP_FRAME_HEIGHT, HEIGHT);
+        Mat frame;
+        vector<uchar> img_buffer;
+
+        while(1){
+            cap >> frame;
+            if(frame.empty()){
+                cout << "[e] Failed to capture frame" << '\n';
+                break;
+            }
+            vector<int> compression_params = { IMWRITE_JPEG_QUALITY, QUALITY };
+            imencode(".jpg", frame, img_buffer, compression_params);
+
+            int bytes_sent = send(client_socket, reinterpret_cast<const char*>(img_buffer.data()), img_buffer.size(), 0);
+            if(bytes_sent == SOCKET_ERROR){
+                cout << "[e] Send failed. Error Code: " << WSAGetLastError() << '\n';
+                break;
+            }
+
+            int bytes_received = recv(client_socket, recv_buffer, BUFFER_SIZE, 0);
+            if(bytes_received > 0){
+                string reply(recv_buffer, bytes_received);
+                if(reply != "400") cout << "[w] Unexpected reply from server\n";
+            }
+            else if(bytes_received == 0){
+                cout << "[w] Connection closed by server\n";
+                break;
+            }
+            else{
+                cout << "[e] Receive failed. Error Code: " << WSAGetLastError() << '\n';
+                break;
+            }
+
+            if(GetAsyncKeyState('Q') & 0x8000){
+                cout << "[i] Shutting down client...\n";
+                closesocket(client_socket);
+                WSACleanup();
+                return 0;
+            }
+        }
+
         closesocket(client_socket);
-        return 1;
+        WSACleanup();
+        cout << "[w] Attempt " << attempt << ". Restarting in 3 seconds...\n";
+        Sleep(3000);
+        attempt++;
     }
-    cout << "Connected to server" << '\n';
-
-    VideoCapture cap(0);
-    if(!cap.isOpened()){
-        cout << "Error opening camera" << '\n';
-        return 1;
-    }
-    cap.set(CAP_PROP_FRAME_WIDTH, WIDTH);
-    cap.set(CAP_PROP_FRAME_HEIGHT, HEIGHT);
-    Mat frame;
-    vector<uchar> img_buffer;
-
-    while(true){
-        cap >> frame;
-        if (frame.empty()){
-            cout << "Failed to capture frame" << '\n';
-            break;
-        }
-        vector<int> compression_params = { IMWRITE_JPEG_QUALITY, QUALITY };
-        imencode(".jpg", frame, img_buffer, compression_params);
-
-        int bytes_sent = send(client_socket, reinterpret_cast<const char*>(img_buffer.data()), img_buffer.size(), 0);
-        if (bytes_sent == SOCKET_ERROR){
-            cout << "Send failed. Error Code: " << WSAGetLastError() << '\n';
-            break;
-        }
-
-        int bytes_received = recv(client_socket, recv_buffer, BUFFER_SIZE, 0);
-        if(bytes_received > 0) {
-            string reply(recv_buffer, bytes_received);
-            if(reply != "400") cout << "Unexpected reply from server\n";
-        }
-        else if(bytes_received == 0) {
-            cout << "Connection closed by server\n";
-            break;
-        }
-        else{
-            cout << "Receive failed. Error Code: " << WSAGetLastError() << '\n';
-            break;
-        }
-    }
-
+    
+    cout << "[i] Shutting down client...\n";
     closesocket(client_socket);
     WSACleanup();
-    waitKey(0);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,8 +1,10 @@
-#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <opencv2/opencv.hpp>
 #include <sys/types.h>
 #include <winsock2.h>
+#include <windows.h>
 #include <chrono>
+#include <iomanip>
 #pragma comment(lib, "ws2_32.lib")
 
 using namespace std;
@@ -18,74 +20,96 @@ int main(){
     WSADATA wsaData;
     SOCKET server_socket, client_socket;
     struct sockaddr_in server_addr, client_addr;
-    int addr_len = sizeof(client_addr);
+    int addr_len = sizeof(client_addr), attempt = 1;
 
-    if(WSAStartup(MAKEWORD(2,2), &wsaData) != 0){
-        cout << "Failed to initialize Winsock. Error Code: " << WSAGetLastError() << '\n';
-        return 1;
-    }
-    if((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET){
-        cout << "Socket creation failed. Error Code: " << WSAGetLastError() << '\n';
-        return 1;
-    }
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
+    while(1){
+        cout << "[i] Initializing server...\n";
 
-    if(::bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR){
-        cout << "Bind failed. Error Code: " << WSAGetLastError() << '\n';
-        closesocket(server_socket);
-        return 1;
-    }
-
-    listen(server_socket, 3);
-    cout << "Waiting for incoming connections...\n";
-
-    if((client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &addr_len)) == INVALID_SOCKET){
-        cout << "Accept failed. Error Code: " << WSAGetLastError() << '\n';
-        closesocket(server_socket);
-        return 1;
-    }
-    cout << "Connection accepted\n";
-
-    char buffer[BUFFER_SIZE];
-    int bytes_received;
-    vector<uchar> img_data;
-    auto prev = high_resolution_clock().now();
-
-    while(true){
-        bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
-        if(bytes_received == SOCKET_ERROR){
-            cout << "Receive failed. Error Code: " << WSAGetLastError() << '\n';
-            break;
+        if(WSAStartup(MAKEWORD(2,2), &wsaData) != 0){
+            cout << "[e] Failed to initialize Winsock. Error Code: " << WSAGetLastError() << '\n';
+            return 1;
         }
-        else if(bytes_received == 0){
-            cout << "Connection closed by client\n";
+        if((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET){
+            cout << "[e] Socket creation failed. Error Code: " << WSAGetLastError() << '\n';
+            return 1;
         }
-        cout << bytes_received/1000.0 << " kB\t";
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_addr.s_addr = INADDR_ANY;
+        server_addr.sin_port = htons(PORT);
 
-        auto curr = high_resolution_clock().now();
-        auto duration = duration_cast<milliseconds>(curr-prev);
-        prev = high_resolution_clock().now();
-        cout << 1000.0/duration.count() << " fps\n";
+        if(::bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR){
+            cout << "[e] Bind failed. Error Code: " << WSAGetLastError() << '\n';
+            closesocket(server_socket);
+            return 1;
+        }
 
-        img_data.insert(img_data.end(), buffer, buffer + bytes_received);
-        if(bytes_received < BUFFER_SIZE){
-            Mat img = imdecode(img_data, IMREAD_COLOR);
-            if(!img.empty()) {
-                imshow("Received image", img);
-                waitKey(1);
+        listen(server_socket, 3);
+        cout << "[i] Waiting for connections...\n";
+
+        if((client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &addr_len)) == INVALID_SOCKET){
+            cout << "[e] Accept failed. Error Code: " << WSAGetLastError() << '\n';
+            closesocket(server_socket);
+            return 1;
+        }
+        cout << "[i] Connection accepted\n";
+
+        char buffer[BUFFER_SIZE];
+        int bytes_received;
+        vector<uchar> img_data;
+        auto prev = high_resolution_clock().now();
+
+        cout << "[i] Waiting for incoming messages...\n";
+        while(1){
+            bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
+            if(bytes_received == SOCKET_ERROR){
+                cout << "[e] Receive failed. Error Code: " << WSAGetLastError() << '\n';
+                break;
             }
-            img_data.clear();
+            else if(bytes_received == 0){
+                cout << "[w] Connection closed by client\n";
+                break;
+            }
+            cout << "[recv] " << fixed << setprecision(2) << bytes_received/1000.0 << " kB\t";
 
-            string reply = "400";
-            send(client_socket, reply.c_str(), reply.size(), 0);
+            auto curr = high_resolution_clock().now();
+            auto duration = duration_cast<milliseconds>(curr-prev);
+            prev = high_resolution_clock().now();
+            cout << "@ " << fixed << setprecision(2) << 1000.0/duration.count() << " fps\n";
+
+            img_data.insert(img_data.end(), buffer, buffer + bytes_received);
+            if(bytes_received < BUFFER_SIZE){
+                Mat img = imdecode(img_data, IMREAD_COLOR);
+                if(!img.empty()) {
+                    imshow("Received image", img);
+                    waitKey(1);
+                }
+                img_data.clear();
+
+                string reply = "400";
+                send(client_socket, reply.c_str(), reply.size(), 0);
+            }
+
+            if(GetAsyncKeyState('Q') & 0x8000){
+                cout << "[i] Shutting down server...\n";
+                closesocket(client_socket);
+                closesocket(server_socket);
+                WSACleanup();
+                return 0;
+            }
         }
+
+        closesocket(client_socket);
+        closesocket(server_socket);
+        WSACleanup();
+
+        cout << "[w] Attempt " << attempt << ". Restarting in 3 seconds...\n";
+        Sleep(3000);
+        attempt++;
     }
 
+    cout << "[i] Shutting down server...\n";
     closesocket(client_socket);
     closesocket(server_socket);
     WSACleanup();
-    waitKey(0);
     return 0;
 }
